b3acbafa79ab59aa0c227dda2e005b49d158310d
FAROOQI <attaurrahmanfarooqi9@gmail.com>
2020-12-09 23:39:32 +0500

    EveryThing DONE

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ function input() {
     num1.push(document.getElementById("userNum1").value);
     num2.push(document.getElementById("userNum2").value);
 
-    // document.getElementById("userNum1").innerHTML = num1;
-    // document.getElementById("userNum2").innerHTML = num2;
+    document.getElementById("userNum1").innerHTML = num1;
+    document.getElementById("userNum2").innerHTML = num2;
 
 
-    console.log(num1)
-    console.log(num2)
+    // console.log(num1)
+    // console.log(num2)
 
 
     compNum1 = num1 - "9999";
@@ -23,13 +23,13 @@ function input() {
     compNum1 = -compNum1;
     compNum2 = -compNum2;
 
-    document.getElementById("comp2").innerHTML = compNum2;
-    document.getElementById("comp1").innerHTML = compNum1;
+    document.getElementById("comp1").innerHTML = "Computer 1st Number: &nbsp;" + compNum1;
+    document.getElementById("comp2").innerHTML = "Computer 2nd Number: &nbsp;" + compNum2;
 
 
     result = (+compNum1 + +compNum2 + +num1 + +num2);
 
-    document.getElementById("result").innerHTML = result;;
+    document.getElementById("result").innerHTML ="Result: &nbsp;" + result;;
 
 
     return false;
